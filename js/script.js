b08117931e4a671144be7c678ab814cba7ba178a
DharanPk <dharanmd369@gmail.com>
2025-12-26 19:41:50 +0530

    task

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// task 1
+var a="10"
+var b=5
+var c=a+b
+console.log(c)
+console.log(typeof c)
+
+// task2
+let x=10
+let y="10"
+let z=x==y 
+console.log(x==y)
+console.log(x===y)
+console.log(typeof z)
+
+// task3 (if else)
+ var num=-10
+if(num>0){
+    console.log("Positive")
+}
+else if(num<0){
+    console.log("Negative")
+}
+else{
+    console.log("Zero")
+}
+// Opertor
+var num=15
+var check=(num>0)?"Positive":"Negative"
+console.log(check)
+
+
+//task4 
+p=10
+q=20
+r="30"
+s=p+q+r
+console.log(s)
+console.log(typeof s)
+
+//task5
+var v=true
+var a=Number(v)+5
+console.log(a)
+console.log(typeof a)
+
+//task6
+var a=7
+var b=3
+var c=a-b
+var q=(c>0)?"greater than a":"not greater tha a"
+var r=(a>b)
+console.log(q)
+console.log(r)
+
+//task7
+var mark="85"
+var pass=(mark>=40)?"passed":"failed"
+console.log(pass)
+console.log(typeof pass)
+
+//task8
+var age=20
+var hasid=false
+var check=(age>=18 && hasid==true)
+console.log(check)
+
+//task9
+var count
+console.log(count)
+console.log(typeof count)
+count=10
+console.log(count)
+console.log(typeof count)
+
+//task10
+var num1="5"
+var num2="2"
+var a
+//add
+ a=num1+num2
+ //sub
+ b=num1-num2
+ //mul
+ c=num1*num2
+ console.log(a)
+ console.log(b)
+ console.log(c)
+  console.log(typeof a)
+ console.log(typeof b)
+ console.log(typeof c)
+
+
+
