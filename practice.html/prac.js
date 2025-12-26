15db172850b4986f11e3a0df9dfb3072b2420abb
DharanPk <dharanmd369@gmail.com>
2025-12-26 19:52:03 +0530

    task

---
+++ practice.html/prac.js
@@ -0,0 +1,44 @@
+
+//  {
+//     // var c=10 
+//     const c=20
+//     console.log(c)
+
+    
+//  }
+
+// var a=2
+// var b=2
+// var c=a**b
+// console.log(c)
+
+
+// var a=10
+// var b=a--
+// console.log(a)
+// console.log(b)
+// console.log(a)
+
+// var a=10
+// var b=10
+// var c=a != 20
+// console.log(c)
+
+// let x=10
+// let y=20
+// let z=(x <0 ||  y<0)
+// console.log(z)
+
+// let a=10
+// let b=20
+// let r=(a<20)?"mm":"pk"
+// console.log("The name is "+ r)
+
+
+// let arr=[a,20,true]
+// console.log(arr)
+
+
+
+
+
