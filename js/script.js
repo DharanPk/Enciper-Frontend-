c30828260505f16e967a031e26fc38b580535275
DharanPk <dharanmd369@gmail.com>
2025-12-29 07:29:02 +0530

    script

--- js/script.js
+++ js/script.js
@@ -1,94 +1,191 @@
-// task 1
-var a="10"
-var b=5
-var c=a+b
-console.log(c)
-console.log(typeof c)
+// // task 1
+// var a="10"
+// var b=5
+// var c=a+b
+// console.log(c)
+// console.log(typeof c)
 
-// task2
-let x=10
-let y="10"
-let z=x==y 
-console.log(x==y)
-console.log(x===y)
-console.log(typeof z)
+// // task2
+// let x=10
+// let y="10"
+// let z=x==y 
+// console.log(x==y)
+// console.log(x===y)
+// console.log(typeof z)
 
-// task3 (if else)
- var num=-10
-if(num>0){
-    console.log("Positive")
-}
-else if(num<0){
-    console.log("Negative")
-}
-else{
-    console.log("Zero")
-}
-// Opertor
-var num=15
-var check=(num>0)?"Positive":"Negative"
-console.log(check)
+// // task3 (if else)
+//  var num=-10
+// if(num>0){
+//     console.log("Positive")
+// }
+// else if(num<0){
+//     console.log("Negative")
+// }
+// else{
+//     console.log("Zero")
+// }
+// // Opertor
+// var num=15
+// var check=(num>0)?"Positive":"Negative"
+// console.log(check)
 
 
-//task4 
-p=10
-q=20
-r="30"
-s=p+q+r
-console.log(s)
-console.log(typeof s)
+// //task4 
+// p=10
+// q=20
+// r="30"
+// s=p+q+r
+// console.log(s)
+// console.log(typeof s)
 
-//task5
-var v=true
-var a=Number(v)+5
-console.log(a)
-console.log(typeof a)
+// //task5
+// var v=true
+// var a=Number(v)+5
+// console.log(a)
+// console.log(typeof a)
 
-//task6
-var a=7
-var b=3
-var c=a-b
-var q=(c>0)?"greater than a":"not greater tha a"
-var r=(a>b)
-console.log(q)
-console.log(r)
+// //task6
+// var a=7
+// var b=3
+// var c=a-b
+// var q=(c>0)?"greater than a":"not greater tha a"
+// var r=(a>b)
+// console.log(q)
+// console.log(r)
 
-//task7
-var mark="85"
-var pass=(mark>=40)?"passed":"failed"
-console.log(pass)
-console.log(typeof pass)
+// //task7
+// var mark="85"
+// var pass=(mark>=40)?"passed":"failed"
+// console.log(pass)
+// console.log(typeof pass)
 
-//task8
-var age=20
-var hasid=false
-var check=(age>=18 && hasid==true)
-console.log(check)
+// //task8
+// var age=20
+// var hasid=false
+// var check=(age>=18 && hasid==true)
+// console.log(check)
 
-//task9
-var count
-console.log(count)
-console.log(typeof count)
-count=10
-console.log(count)
-console.log(typeof count)
+// //task9
+// var count
+// console.log(count)
+// console.log(typeof count)
+// count=10
+// console.log(count)
+// console.log(typeof count)
 
-//task10
-var num1="5"
-var num2="2"
-var a
-//add
- a=num1+num2
- //sub
- b=num1-num2
- //mul
- c=num1*num2
- console.log(a)
- console.log(b)
- console.log(c)
-  console.log(typeof a)
- console.log(typeof b)
- console.log(typeof c)
+// //task10
+// var num1="5"
+// var num2="2"
+// var a
+// //add
+//  a=num1+num2
+//  //sub
+//  b=num1-num2
+//  //mul
+//  c=num1*num2
+//  console.log(a)
+//  console.log(b)
+//  console.log(c)
+//   console.log(typeof a)
+//  console.log(typeof b)
+//  console.log(typeof c)
+
+
+//  var i=5;
+//  do{
+//     console.log(i);
+//     i++;
+//  }while(i<5)
+
+
+//  var i=10;
+//  while(i<5){
+//     console.log(i);
+//     i++;
+//  }
+
+//  for(var i=0;i<10;i++){
+//     console.log(i);
+//  }
+
+// const a=20
+// const b=10
+// function add(){
+//     console.log(a+b)
+// }
+// add()
+
+// const s= function(){
+//     console.log(a-b)
+// }
+// s()
+
+
+// const person={
+//     name:"Dharan",
+//     age:18
+// }
+// person["data"]={
+//     dept:"CSE",
+//     year:"IV"
+// }
+// person["job"]={
+//     domine:"Frontend",
+//     location:"chennai"
+// }
+// console.log(person) 
+
+
+
+
+// const pro=(a,b) =>console.log((a+b))
+// pro(33,7)
+
+// const r = (n) => {
+//   let d =(n%2== 0 )?"Odd":"even"
+//   return d;
+// };
+
+// console.log(r(2));
+
+// var a=18
+// var b=7
+
+// function add(a,b){
+//     return(a+b)
+// }
+// console.log(add(18,7))
+
+// const r=function(a,b){
+//     return(a-b)
+// }
+// console.log(r(18,45))
+
+
+
+// var n=prompt("enter the number")
+// for(var i=0;i<=n;i++){
+//   if(i%2==0){
+//     s=i
+//   }
+// }
+
+
+// var user=prompt("Name")
+// var age=prompt("Age")
+// console.log(user)
+// console.log(" Name "+ user +" Age "+age)
+// document.getElementById("h1").innerHTML=user
+// document.getElementById("h2").innerHTML=age
+
+
+// var user=prompt("Name")
+// var age=prompt("Age")
+// var object=[]
+// object[0]=user;
+// object[1]=age;
+
+// console.log(object)
 
 
 
---
+++ js/src.js
@@ -0,0 +1,285 @@
+// task1
+var user=prompt("Enter the Name")
+function target(){
+    alert("Welcome to User's page")
+}
+target()
+
+
+
+//task2
+var num=prompt("enter the number")
+function oddeven(){
+    let n=(num%2==0)?"Even":"odd"
+    alert(n)
+}
+oddeven()
+
+
+
+//task3
+var a=Number(prompt("Enter the A Value"))
+var b=Number(prompt("Enter the B Value"))
+var c=prompt("Type the Operator (+,-*,/)")
+switch(c){
+    case "+":
+        alert(a+b)
+        break;
+    case "-":
+        alert(a-b)
+        break;
+    case "*":
+        alert(a*b)
+        break; 
+    case "/":
+        alert(a/b)
+        break;           
+    default:
+        alert("Invalid Operator")
+        break;
+}
+
+
+
+// task4
+var a=prompt("Enter the User's Age")
+function age(){
+    if(a>=18){
+        alert("Eligible to Vote")
+    }
+    else{
+        alert("Not Eligible to Vote")
+    }
+}
+age()
+
+
+
+// task5
+var number=prompt("Enter the Number")
+for(var i=1;i<=10;i++){
+    console.log(i+"*"+ number+" = " +(number*i))
+}
+
+
+
+
+// task6
+var n=prompt("Enter the Limit Numberfor sum of Number")
+var sum=0;
+for(var i=1;i<=n;i++){
+    sum+=i
+}
+alert(sum)
+
+
+
+//task7
+var pass=prompt("Enter the Password")
+if(pass.length >=10){
+    alert("PASSWORD VALID")
+}
+else{
+    alert("INVALID [Above 10 character]")
+}
+
+
+
+// task8
+var mark=prompt("Enter the OVERALL MARK")
+if(mark>90){
+    alert("Grade A")
+}
+else if(mark>80){
+    alert("Grade B")
+}
+else if(mark>60){
+    alert("Grade C")
+}
+else if(mark>50){
+   alert("Grade D")
+}
+else{
+    alert("Fail")
+}
+
+
+
+// task9
+var a=prompt("Enter the value of A")
+var b=prompt("Enter the value of B")
+var c=prompt("Enter the value of C")
+if(a>b && a>c){
+    alert("A is Larger")
+}
+else if(b>a && b>c){
+    alert("B is Larger")
+}
+else{
+    alert("C is larger")
+}
+
+
+
+// task10
+var user="Dharaneesh"
+var pass="Dharan@2004"
+let nam=prompt("USERNAME")
+let key=prompt("PASSWORD")
+if(user.length==nam.length && user==nam && pass.length ==key.length && pass==key ){
+    alert("Sucessfully Login")
+}
+else{
+    alert("Unsucessfull")
+}
+
+
+
+// task11
+var n=prompt("Enter the Limit for Even Count")
+var c=0;
+  for(var i=1;i<=n;i++){
+    if(i%2==0){
+     c+=i;
+ }
+}
+ alert(c)
+
+
+
+//  task12
+var n=prompt("Enter the number for Reverse a Number")
+var rev=" "
+for(var i=n;i>0;i--){
+      rev+=i+"  "
+}
+alert(rev)
+
+
+// task13
+var amt=Number(prompt("Enter the Principal Amount"))
+var rate=Number(prompt("Enter the  Rate of Amount"))
+var time=Number(prompt("Enter the Time"))
+const simple=(amt,rate,time)=>{ let ans=amt*rate*time/100
+alert(ans)
+}
+simple(amt,rate,time)
+
+
+
+// task14
+var fix=18
+var user=prompt("Enter The Correct Number")
+if(user>fix){
+    alert("TO Hight")
+}
+else if(user<fix){
+    alert("Too Low")
+}
+else{
+    alert("FINDED")
+}
+
+// task15
+var n=prompt("Enter the Number")
+const prime= n =>{
+let f=0;
+for(let i=2;i<n;i++){
+    if(n%i==0){
+        f++;
+    }
+}
+if(f==0){
+    alert(n +" Prime Number")
+}
+else{
+    alert(n +" Not a Prime")
+}
+}
+prime(n)
+
+
+
+// task16
+var n=prompt("Enter the Number")
+function factorial(n){
+var fact=1
+for(let i=1;i<=n;i++){
+    fact*=i
+}
+alert(fact)
+}
+factorial(n)
+
+
+
+// task17
+var n=prompt("Enter the Number to Count Digits")
+function count(n){
+    let c=0
+    while(n!=0){
+        let x=n%10
+        c++;
+        n=Math.floor(n/10);
+    }
+    alert(c)
+}
+count(n)
+
+
+
+// task18 
+const menu = prompt("MENU\n"+"1.Add\n"+"2.Subract\n"+"3.Multiply\n"+"4.Divide\n"+"Enter the Option (1-4) :    ")      
+const a = Number(prompt("Enter the A number"));
+const b = Number(prompt("Enter the B number"));
+
+const add=(a, b)=>  a + b;
+
+const sub=(a, b)=> a - b;
+
+const mul=(a, b)=> a * b;
+
+const div=(a, b) => a / b;
+
+switch (menu) {
+case "1":
+alert(add(a,b));
+break;
+case "2":
+alert(sub(a,b));
+break;
+case "3":
+alert(mul(a,b));
+break;
+case "4":
+alert(div(a,b));
+break;
+default:
+alert("Inavlid Operation Choose 1 to 4");
+}
+
+
+
+// task19
+var l=prompt("Enter the Limit")
+let even=0
+let odd=0
+for(var i=1;i<=l;i++){
+    if(i%2==0){
+        even+=i
+    }
+    else{
+        odd+=i
+    }
+}
+// alert(even)
+// alert(odd)
+alert(even+" = Event "+ odd+ " = odd ")
+
+// task20
+var user=confirm("Do you want to Exit?");
+    if(user){
+        alert("Exit successfully");
+    }else{
+        alert("Continue to work");
+    }
