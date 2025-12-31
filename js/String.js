1c5f9c94ff03b4971b82ce7f0f0600f43a5fd96c
DharanPk <dharanmd369@gmail.com>
2025-12-31 07:26:28 +0530

    practice

---
+++ js/String.js
@@ -0,0 +1,77 @@
+// var text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// console.log( text.length);
+
+// var text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text);
+
+// let text = "HELLO WORLD";
+// let a = text.charAt(0);
+// let b = text.charCodeAt(0);
+// let c = text.codePointAt(0);
+// let d = text[0];
+// console.log(a+" "+b+" "+c+" "+d)
+
+
+// let t1 = "Hello";
+// let t2 = "World";
+// let t3 = t1.concat(" ", t2);
+
+
+// let word = "virat,Dhoni,rohit";
+// let p1 = word.slice(0,5 );
+// let p2 = word.slice(-11);//
+
+// let sub = word.substring(12, 17);//no negative allow
+// let slice = word.substr(0, 5);//length
+
+// console.log(p1+" "+p2);
+// console.log(sub+" "+slice);
+
+
+// let w1 = "Hogwarts";
+// let w2 = w1.toUpperCase();
+// let w3 = w1.toLowerCase(); 
+// console.log(w1+" "+ w2)
+
+
+// let tr1 = " Money Hiest  ";
+// let tr2 = tr1.trim().length()
+// let tr3 = tr1.trimStart().length()
+// let tr4 = tr1.trimEnd().length()
+
+// console.log(tr2+" "+tr3+" "+tr4);
+
+// let num = "5";
+// let pad1 = num.padStart(4,"0");
+// let pad2 = num.padEnd(4,"0");
+// console.log(pad1);
+// console.log(pad2);
+
+
+// let sum=5            //number convert to String after pad it
+// let str=sum.toString()
+// let pad3 = str.padStart(4,"0");
+// console.log(pad3);
+
+// let text = "Gryffindor";
+// let r = text.repeat(2);
+// console.log(r);
+
+
+let par="Welcome to javascript code";
+let n= par.replace("code","page");
+console.log(n)
+
+
+let par1="Welcome to javascript code";
+let n1= par1.replace(/CODE/i,"page");
+console.log(n1)
+
+
+
+
+let par2="Welcome to javascript code and html and css code";
+let n2= par2.replace(/code/g,"page");
+console.log(n2)
+
+
